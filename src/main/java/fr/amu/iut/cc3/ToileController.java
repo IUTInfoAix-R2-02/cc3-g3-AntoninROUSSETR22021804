0462d9d912b9bd5c5383972e61afafd7d5f4d5cf
r22021804 <antonin.rousset@etu.univ-amu.fr>
2023-06-02 16:15:51 +0200

    rendu 1

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -12,11 +12,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -31,10 +35,29 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
+    @FXML
+    private TextField comp1;
+    @FXML
+    private TextField comp2;
+    @FXML
+    private TextField comp3;
+    @FXML
+    private TextField comp4;
+    @FXML
+    private TextField comp5;
+    @FXML
+    private TextField comp6;
+    @FXML
+    private Pane pane;
+    @FXML
+    private Label E1;
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
+
 
     int getXRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
@@ -46,4 +69,99 @@ public class ToileController implements Initializable {
                 *  (value / noteMaximale));
     }
 
+    public void changerPosCercle(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp1.getText())>=0) && (Double.parseDouble(comp1.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp1.getText()), 1));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp1.getText()), 1));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
+
+    public void changerPosCercle6(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp6.getText())>=0) && (Double.parseDouble(comp6.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp6.getText()), 6));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp6.getText()), 6));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
+
+    public void changerPosCercle5(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp5.getText())>=0) && (Double.parseDouble(comp5.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp5.getText()), 5));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp5.getText()), 5));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
+
+    public void changerPosCercle4(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp4.getText())>=0) && (Double.parseDouble(comp4.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp4.getText()), 4));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp4.getText()), 4));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
+
+    public void changerPosCercle3(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp3.getText())>=0) && (Double.parseDouble(comp3.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp3.getText()), 3));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp3.getText()), 3));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
+
+    public void changerPosCercle2(ActionEvent actionEvent) {
+        if ((Double.parseDouble(comp2.getText())>=0) && (Double.parseDouble(comp2.getText())<=20)){
+            E1.setVisible(false);
+            Circle cercle = new Circle();
+            cercle.setRadius(5);
+            cercle.setStroke(Color.BLACK);
+            cercle.setCenterY(getYRadarChart(Double.parseDouble(comp2.getText()), 2));
+            cercle.setCenterX(getXRadarChart(Double.parseDouble(comp2.getText()), 2));
+            pane.getChildren().add(cercle);
+
+        }
+        else {
+            E1.setVisible(true);
+        }
+    }
 }
